1b7fbad67fa22ba22bac41a6990bb7fcf8af65d6
Paulo McNally <paulomcnally@gmail.com>
2014-08-29 17:15:20 -0600

    remove line

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,27 +5,12 @@
  * Copyright (c) 2014 Paulo McNally
  * Licensed under the MIT license.
  */
-
 'use strict';
-
 module.exports = function (grunt) {
   // load all npm grunt tasks
   require('load-grunt-tasks')(grunt);
-
   // Project configuration.
   grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-        reporter: require('jshint-stylish')
-      }
-    },
-
     // Configuration to be run (and then tested).
     docs: {
       'default_options': {
@@ -34,17 +19,9 @@ module.exports = function (grunt) {
         }
       }
     }
-
   });
-
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['docs']);
-
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['test']);
-
+  grunt.registerTask('default', ['docs']);
 };
