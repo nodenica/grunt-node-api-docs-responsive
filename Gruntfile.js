130cb7876f4e41d7bc541a5fd2bbba400776ff5a
Paulo McNally <paulomcnally@gmail.com>
2014-09-17 00:45:28 -0600

    0.0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,25 @@ module.exports = function (grunt) {
     docs: {
       'default_options': {
         options: {
-          version: '0.10.32'
-        }
+          version: '0.10.32',
+          dest: './dest/'
+        },
+
+      }
+    },
+    // copy files
+    copy: {
+      files: {
+        cwd: './assets',  // set working folder / root to copy
+        src: '**/*',           // copy all files and subfolders
+        dest: './dest/assets',    // destination folder
+        expand: true           // required when using cwd
       }
     }
   });
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['docs']);
+  grunt.registerTask('default',['docs', 'copy']);
 };
